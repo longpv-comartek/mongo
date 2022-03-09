2b1c89b4ea942cf08c88cc4d1112ad72c4f65ca9
long <bekeo0302@gmail.com>
2022-03-09 09:19:27 +0700

    mongo

--- src/subject/subject.controller.ts
+++ src/subject/subject.controller.ts
@@ -1,4 +1,74 @@
-import { Controller } from '@nestjs/common';
+import { SubjectService } from './subject.service';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpException,
+    HttpStatus,
+    Patch,
+    Post,
+    Query,
+} from '@nestjs/common';
+import {
+    CreateSubjectDto,
+    DeleteSubjectDto,
+    UpdateSubjectDto,
+} from './dto';
+import { ScoreService } from '../score/score.service'
 
 @Controller('subject')
-export class SubjectController {}
+
+export class SubjectController {
+
+    constructor(
+        private readonly SubjectService: SubjectService,
+        private readonly ScoreService: ScoreService) { }
+
+    @Post()
+    async create(@Body() id: string, createClassDto: CreateSubjectDto) {
+        const score = this.ScoreService.findOne(id);
+        if (!score) {
+            throw new HttpException({
+                status: HttpStatus.BAD_REQUEST,
+                error: `Bad Request: subject cannot found!`,
+            }, HttpStatus.BAD_REQUEST);
+        }
+        return this.SubjectService.create(createClassDto);
+    }
+
+    @Patch()
+    async update(@Body() id: string, updateClassDto: UpdateSubjectDto) {
+        const score = this.ScoreService.findOne(id);
+        if (!score) {
+            throw new HttpException({
+                status: HttpStatus.BAD_REQUEST,
+                error: `Bad Request: subject cannot found!`,
+            }, HttpStatus.BAD_REQUEST);
+        }
+        return this.SubjectService.update(updateClassDto);
+    }
+
+    @Delete()
+    async delete(@Body() id: string, deleteClassDto: DeleteSubjectDto) {
+        const score = this.ScoreService.findOne(id);
+        if (!score) {
+            throw new HttpException({
+                status: HttpStatus.BAD_REQUEST,
+                error: `Bad Request: subject cannot found!`,
+            }, HttpStatus.BAD_REQUEST);
+        }
+        return this.SubjectService.delete(deleteClassDto);
+    }
+
+    @Get()
+    async all(@Query() id: string) {
+        return this.SubjectService.findOne(id);
+    }
+
+    @Get()
+    async search() {
+        return this.SubjectService.findAll();
+    }
+
+}
--- src/subject/subject.module.ts
+++ src/subject/subject.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { SubjectService } from './subject.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SubjectSchema, Subject } from '../subject/schema/subject.schema'
+import { ScoreModule } from 'src/score/score.module';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Subject.name, schema: SubjectSchema }]),
+    ScoreModule
   ],
   providers: [SubjectService],
   exports: [SubjectService]
